922f899f059832575375408eb4dfdf0a2219ac2d
matheuscs123 <114266737+matheuscs123@users.noreply.github.com>
2022-09-23 23:09:24 -0300

    Add files via upload

---
+++ q5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float vet[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    float *f;
+    int i;
+
+    f = vet;
+    printf("Contador | Valor | Valor | Endereco | Endereco \n");
+
+    for(i=0; i<=4; i++){
+        printf("i = %d ", i);
+        printf("| vet[%d] = %.1f ", i, vet[i]);
+        printf("| *(f + %d) = %.1f ", i, *(f+i));
+        printf("| &vet[%d] = %X ", i, &vet[i]);
+        printf("| (f + %d) = %X \n", i, f+i);
+    }
+
+    return 0;
+}
